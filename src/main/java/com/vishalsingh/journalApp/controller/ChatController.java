144f15d18811dcfb3eb1e97ce35389d44ec2ae67
vishal singh <vishalbalipur1212@gmail.com>
2025-09-25 23:39:55 +0530

    hello

--- src/main/java/com/vishalsingh/journalApp/controller/ChatController.java
+++ src/main/java/com/vishalsingh/journalApp/controller/ChatController.java
@@ -1,11 +1,10 @@
 package com.vishalsingh.journalApp.controller;
 
 import com.vishalsingh.journalApp.Api.responce.ChatResponce;
+import com.vishalsingh.journalApp.entity.ChatEntity;
 import com.vishalsingh.journalApp.service.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/chat")
@@ -14,8 +13,8 @@ public class ChatController {
     @Autowired
     private ChatService chatService;
     @GetMapping
-    public String infor(){
-        ChatResponce response = chatService.chatInfo("Narendra_Modi");//.getQuery().getPages().values().iterator().next();
+    public String infor(@RequestParam String topic){
+        ChatResponce response = chatService.chatInfo(topic);//.getQuery().getPages().values().iterator().next();
         String extract = null;
         if (response.getQuery() != null && response.getQuery().getPages() != null && !response.getQuery().getPages().isEmpty()) {
             ChatResponce.Page firstPage = response.getQuery().getPages().values().iterator().next();
---
+++ src/main/java/com/vishalsingh/journalApp/entity/ChatEntity.java
@@ -0,0 +1,8 @@
+package com.vishalsingh.journalApp.entity;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@EntityScan
+public class ChatEntity {
+    public String topic;
+}
